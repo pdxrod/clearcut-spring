bfca0b6c9c388ea9d202cf11f8f408b17807e0fb
z <x@y.com>
2019-02-03 08:02:59 -0800

    Beginning of tests

---
+++ src/test/java/clearcut/PersonTest.java
@@ -0,0 +1,23 @@
+package clearcut;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+import java.sql.SQLException;
+import java.lang.reflect.Constructor;
+
+import org.junit.Test;
+import junit.framework.TestCase;
+import static org.junit.Assert.*;
+
+public class PersonTest extends TestCase {
+
+	public PersonTest() {
+	}
+
+	public void testPerson() throws Exception {
+		assertEquals(true, ! false);
+	}
+
+}
