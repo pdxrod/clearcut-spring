df402a4806e78a5b7ea83c13e8330160be8403df
z <x@y.com>
2019-02-04 12:23:12 -0800

    Added database properties reader to test, still failing on assertTrue( iterator.hasNext() );

--- src/test/java/clearcut/PersonTest.java
+++ src/test/java/clearcut/PersonTest.java
@@ -5,6 +5,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.Properties;
+import java.io.File;
+import java.io.InputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.SQLException;
 import java.sql.DriverManager;
 import java.sql.Connection;
@@ -14,6 +19,8 @@ import java.sql.PreparedStatement;
 import java.sql.Array;
 import javax.sql.DataSource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.junit.Test;
 import junit.framework.TestCase;
 import static org.junit.Assert.*;
@@ -54,36 +61,57 @@ public class PersonTest extends TestCase {
           person.setName( "Fred Wick" );
           assertEquals( person.getFirstName(), "Fred" );
           assertEquals( person.getLastName(), "Wick" );
-          personRepository.save( person );
+          Person saved = personRepository.save( person );
+          assertEquals( saved, person );
+          new TestHelper().showPeople();
 // How to find out if there are any rows using Java
           Iterable<Person> people = personRepository.findAll();
           Iterator<Person> iterator = people.iterator();
-          assertTrue( iterator.hasNext() ); 
+          assertTrue( iterator.hasNext() );
           assertEquals( count + 1, personRepository.count() );
   }
 
-  private int showPeople() throws SQLException {
-    Connection con = null;
-    int count = 0;
-    try {
-      con = DriverManager.getConnection("jdbc:mysql://localhost:3306/springboot_simple", "root", "");
-      String sql = "select first_name, last_name from person";
-			PreparedStatement statement = con.prepareStatement( sql );
-			ResultSet resultset = statement.executeQuery();
-      resultset.first();
-      while( resultset.next() ) {
-        String firstName = resultset.getString( "first_name" );
-        String lastName = resultset.getString( "last_name" );
-        System.out.println( "\n\n\n\n************************ "+firstName+" "+lastName);
-        ++ count;
-      }
-    } finally {
+  private class TestHelper {
+    private Logger log = LoggerFactory.getLogger(TestHelper.class);
+
+    public int showPeople() throws SQLException, IOException {
+      int count = 0;
+      File file = new File(".");
+      String projectPath = file.getAbsolutePath();
+      String propsFile = projectPath + "/src/main/resources/" + "application.properties";
+      file = new File(propsFile);
+      if( ! file.exists() ) throw new IOException( "File " + propsFile + " does not exist" );
+      Properties prop = new Properties();
+      FileInputStream inStream = new FileInputStream(file);
+      prop.load( inStream );
+      Connection con = null;
+
       try {
-        con.close();
-      } catch (Exception e) {
+        String url = prop.getProperty("spring.datasource.url");
+        String username = prop.getProperty("spring.datasource.username");
+        String password = prop.getProperty("spring.datasource.password");
+        con = DriverManager.getConnection(url, username, password);
+        String sql = "select first_name, last_name from person";
+  			PreparedStatement statement = con.prepareStatement( sql );
+  			ResultSet resultset = statement.executeQuery();
+        resultset.beforeFirst();
+        while( resultset.next() ) {
+          String firstName = resultset.getString( "first_name" );
+          String lastName = resultset.getString( "last_name" );
+          log.info( "\n************************ "+firstName+" "+lastName);
+          ++ count;
+        }
+        if( count < 1 ) log.info( "\n\n\n\n*******************\n" +
+                                          "THERE'S NOBODY HOME\n" );
+
+      } finally {
+        try {
+          con.close();
+        } catch (Exception e) {
+        }
       }
+      return count;
     }
-    return count;
   }
 
 }
