c3752b729f93e9f697553cfbdb9ebaa162e02141
z <x@y.com>
2019-02-05 13:56:53 -0800

    Added this - https://spring.io/guides/gs/consuming-rest/ - and it worked first time - it shows a random string when it starts up - and the REST methods still work, e.g. 'curl http://localhost:8080/all'

--- src/main/java/clearcut/Application.java
+++ src/main/java/clearcut/Application.java
@@ -3,11 +3,33 @@ package clearcut;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.batch.BatchAutoConfiguration;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication(exclude = BatchAutoConfiguration.class)
 public class Application {
 
+  private static final Logger log = LoggerFactory.getLogger(Application.class);
+
   public static void main(String[] args) throws Exception {
           SpringApplication.run(Application.class, args);
+  }
+
+  @Bean
+  public RestTemplate restTemplate(RestTemplateBuilder builder) {
+    return builder.build();
+  }
+
+  @Bean
+  public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+    return args -> {
+      Quote quote = restTemplate.getForObject(
+          "http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+      log.info(quote.toString());
+    };
   }
 }
