269fbd47234f941f4111de62954891eb279e22f3
z <x@y.com>
2019-02-05 11:44:12 -0800

    Gave up trying to use all of BatchConfiguration inside GreetingController - just used its writer and its processor to put data from CSV file in database

--- src/main/java/clearcut/PersonItemProcessor.java
+++ src/main/java/clearcut/PersonItemProcessor.java
@@ -15,7 +15,6 @@ public class PersonItemProcessor implements ItemProcessor<Person, Person> {
           final String lastName = person.getLastName().toUpperCase();
 
           final Person transformedPerson = new Person(firstName, lastName);
-
           log.info("Converting (" + person + ") into (" + transformedPerson + ")");
 
           return transformedPerson;
